d64845e1c73d05b1bf890e6e6bd8655f9bf413a6
Mario Rossi <veontomo@gmail.com>
2015-12-18 22:36:42 +0100

    calculate wrapBox method

--- src/reindeer/Reindeer.java
+++ src/reindeer/Reindeer.java
@@ -4,11 +4,8 @@
 package reindeer;
 
 import day1.SantaFloors;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import day2.ElvesBoxes;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -20,9 +17,13 @@ public class Reindeer {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        SantaFloors sf = new SantaFloors();
-        System.out.println(sf.maxFloor("input.txt"));
-        System.out.println(sf.getToFloor("input.txt", -1));
+//        SantaFloors sf = new SantaFloors();
+//        System.out.println(sf.maxFloor("input.txt"));
+//        System.out.println(sf.getToFloor("input.txt", -1));
+        ElvesBoxes boxes = new ElvesBoxes();
+        //System.out.println(boxes.wrap("boxes.txt"));
+        System.out.println(boxes.ribbon("boxes.txt"));
+        
     }
     
 }
