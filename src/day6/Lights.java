b3c142f170df1ebb2eee7edd7f527d240f6e2b98
Mario Rossi <veontomo@gmail.com>
2015-12-21 12:53:33 +0100

    feat: start real problem

--- src/day6/Lights.java
+++ src/day6/Lights.java
@@ -5,6 +5,11 @@
  */
 package day6;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -56,6 +61,9 @@ public class Lights {
      * @param y2
      */
     public void turnOff(int x1, int y1, int x2, int y2) {
+        if (size() == 0){
+            return;
+        }
         int y = y1;
         int x;
         while (y <= y2) {
@@ -123,6 +131,7 @@ public class Lights {
      * @param y
      */
     public void turnOff(int x, int y) {
+        //System.out.println("turn off " + x + ", " + y);
         String token = coordToString(x, y);
         if (lightsOn.contains(token)) {
             lightsOn.remove(token);
@@ -159,19 +168,74 @@ public class Lights {
     public int size() {
         return lightsOn.size();
     }
-    
-    
-    public void execute(String comand){
-//        String operation = parseOperation(comand);
+
+    public void execute(String command) throws Exception {
+        String operation = parseOperation(command);
+        if (operation == null) {
+            throw new Exception("Failed to get command from input" + command);
+        }
+        int[] coords = parseCoordinates(command);
+        if (coords == null) {
+            throw new Exception("Failed to get coordinates from command " + command);
+        }
+        int x1 = coords[0], y1 = coords[1], x2 = coords[2], y2 = coords[3];
+        
+        switch (operation) {
+            case "turn on":
+                turnOn(x1, y1, x2, y2);
+                break;
+            case "turn off":
+                turnOff(x1, y1, x2, y2);
+                break;
+            case "toggle":
+                toggle(x1, y1, x2, y2);
+                break;
+            default:
+                throw new Exception("Operation " + operation + " is not understud in \"" + command + "\"");
+
+        }
+        System.out.println("The number of lights: " + size());
     }
-    
-    public String parseOperation(String comand){
+
+    public String parseOperation(String command) {
         Pattern p = Pattern.compile("^(turn on|turn off|toggle)");
-        Matcher m = p.matcher(comand);
-        if (m.find()){
+        Matcher m = p.matcher(command);
+        if (m.find()) {
             return m.group(1);
         }
         return null;
+    }
+
+    public int[] parseCoordinates(String command) {
+        Pattern p = Pattern.compile(".*?(\\d{1,}),\\s?(\\d{1,}).*?(\\d{1,}),\\s?(\\d{1,})");
+        Matcher m = p.matcher(command);
+        if (m.find() && m.groupCount() == 4) {
+            return new int[]{Integer.parseInt(m.group(1)), Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3)), Integer.parseInt(m.group(4))};
+        }
+        return null;
+
+    }
+
+    public int executeFile(String fileName) throws IOException {
+        File fin = new File(fileName);
+        FileInputStream fis = new FileInputStream(fin);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+
+        String line;
+        int total = 0;
+        while ((line = br.readLine()) != null) {
+            try {
+                System.out.println("Command: " + line);
+                execute(line);
+                
+            } catch (Exception ex) {
+                System.out.println("Exceptoin in executing " + line + ": " + ex.getMessage());
+                break;
+            }
+
+        }
+        br.close();
+        return total;
     }
 
 }
